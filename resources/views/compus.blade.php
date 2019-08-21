87a60f22e3de74ebc8ac18607a0f1417857cecf1
Maca <macca.armjo@gmail.com>
2019-08-21 19:41:19 -0300

    pagina computadoras

--- resources/views/compus.blade.php
+++ resources/views/compus.blade.php
@@ -76,41 +76,6 @@
   </div>
 </div>
 
-<!-- TERCER producto -->
-<div class="contItems d-md-flex flex-md-equal">
-  <div class="img-zone col-md-4 text-center overflow-hidden">
-    <div class="padding js-displayHoverImg">
-      <a name="image" class="itemImg" href="#">
-        <img src="/images/fotosDH/lenovo.jpg" class="img-fluid rounded w-50 h-50" alt="asus">
-      </a>
-    </div>
-  </div>
-
-    <div class="desc_zone col-md-4 text-center overflow-hidden">
-      <div class="itemHeader">
-        <h4><a href="#" class="link">
-          <span itemprop="brand">LENOVO</span>
-          <span itemprop="name">modelo 2</span>
-        </a></h4>
-      </div>
-      <div class="sect">
-        <div class="descripcion">
-          <h3>Descripcion</h3>
-          <ul class="text-left">
-            <li>compu algo</li>
-            <li>2da desc</li>
-            <li>3ra desc</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="buy-zone col-md-4 text-center overflow-hidden">
-    <div class="prices text-center"><span class="price-you-pay bold">$24000</span></div>
-    <div class="boton text-center"><a class="btn btn-outline-secondary" href="{{ url('/audio') }}">Agregar al carrito</a></div>
-  </div>
-</div>
-
 <!-- 4to producto -->
 <div class="contItems d-md-flex flex-md-equal">
   <div class="img-zone col-md-4 text-center overflow-hidden">
@@ -147,7 +112,75 @@
   </div>
 </div>
 
+<!-- producto -->
+<div class="contItems d-md-flex flex-md-equal">
+  <div class="img-zone col-md-4 text-center overflow-hidden">
+    <div class="padding js-displayHoverImg">
+      <a name="image" class="itemImg" href="#">
+        <img src="/images/fotosDH/microsoft-13.jpg" class="img-fluid rounded w-50 h-50" alt="asus">
+      </a>
+    </div>
+  </div>
 
+    <div class="desc_zone col-md-4 text-center overflow-hidden">
+      <div class="itemHeader">
+        <h4><a href="#" class="link">
+          <span itemprop="brand">Microsoft 13</span>
+          <span itemprop="name">modelo</span>
+        </a></h4>
+      </div>
+      <div class="sect">
+        <div class="descripcion">
+          <h3>Descripcion</h3>
+          <ul class="text-left">
+            <li>compu algo</li>
+            <li>2da desc</li>
+            <li>3ra desc</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="buy-zone col-md-4 text-center overflow-hidden">
+    <div class="prices text-center"><span class="price-you-pay bold">$50000</span></div>
+    <br>
+    <div class="boton text-center"><a class="btn btn-outline-secondary" href="{{ url('/audio') }}">Agregar al carrito</a></div>
+  </div>
+</div>
+<div class="contItems d-md-flex flex-md-equal">
+  <div class="img-zone col-md-4 text-center overflow-hidden">
+    <div class="padding js-displayHoverImg">
+      <a name="image" class="itemImg" href="#">
+        <img src="/images/fotosDH/microsoft.jpg" class="img-fluid rounded w-50 h-50" alt="asus">
+      </a>
+    </div>
+  </div>
+
+    <div class="desc_zone col-md-4 text-center overflow-hidden">
+      <div class="itemHeader">
+        <h4><a href="#" class="link">
+          <span itemprop="brand">Microsoft</span>
+          <span itemprop="name">modelo 2017</span>
+        </a></h4>
+      </div>
+      <div class="sect">
+        <div class="descripcion">
+          <h3>Descripcion</h3>
+          <ul class="text-left">
+            <li>compu algo</li>
+            <li>2da desc</li>
+            <li>3ra desc</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="buy-zone col-md-4 text-center overflow-hidden">
+    <div class="prices text-center"><span class="price-you-pay bold">$50000</span></div>
+    <br>
+    <div class="boton text-center"><a class="btn btn-outline-secondary" href="{{ url('/audio') }}">Agregar al carrito</a></div>
+  </div>
+</div>
 
 
 
