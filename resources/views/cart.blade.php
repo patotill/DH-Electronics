206cac5decdf0dc8e6a0d67127839b4c631aea70
Maca <macca.armjo@gmail.com>
2019-08-31 19:23:29 -0300

    falta arreglar foreach-carrito

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -21,28 +21,28 @@
 								<div class="row">
 									<div class="col-sm-2 hidden-xs"><img src="{{ Storage::url($product->image) }}" alt="..." style="max-width: 100px;				max-height: 100px;" class="img-responsive"/></div>
 									<div class="col-sm-10">
-										<h4 class="nomargin"></h4>
-										<p></p>
+										<h4 class="nomargin">{{ $product->name}}</h4>
+										<p>{{ $product->description }}</p>
 									</div>
 								</div>
 							</td>
-							<td data-th="Price"></td>
+							<td data-th="Price">{{ $product->price }}</td>
 							<td data-th="Quantity">
 								<input type="number" class="form-control text-center" value="1">
 							</td>
-							<td data-th="Subtotal" class="text-center"></td>
+							<td data-th="Subtotal" class="text-center">{{ $product->price }}</td>
 							<td class="actions" data-th="">
 								<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
 								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
 							</td>
 						</tr>
-
+					
 					</tbody>
 					<tfoot>
 						<tr>
 							<td><a href="{{ url()->previous() }}" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
 							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Total </strong></td>
+							<td class="hidden-xs text-center"><strong>Total ${{ $product->price }}</strong></td>
 							<td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
 						</tr>
 					</tfoot>
